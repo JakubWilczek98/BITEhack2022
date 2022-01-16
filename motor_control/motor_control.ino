f527502a56a1f191956ba855b6ae9c0b303b1f07
Pawel W <pawiercioch@gmail.com>
2022-01-16 06:52:11 +0100

    Add server with info about robot

--- motor_control/motor_control.ino
+++ motor_control/motor_control.ino
@@ -1,3 +1,52 @@
+#ifdef ESP32
+  #include <WiFi.h>
+  #include <ESPAsyncWebServer.h>
+#else
+  #include <Arduino.h>
+  #include <ESP8266WiFi.h>
+  #include <Hash.h>
+  #include <ESPAsyncTCP.h>
+  #include <ESPAsyncWebServer.h>
+#endif
+
+
+// Zamienień na&nbsp;własne dane WiFi
+const char* ssid = "BITEhack2022";
+const char* password = "hackathon";
+
+// Stworzenie obiektu AsyncWebServer na&nbsp;porcie 80
+AsyncWebServer server(80);
+
+
+// HTML code
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+
+</head>
+<body>
+  <h2>Serwer ESP DS18B20</h2>
+  <p>
+    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i>
+    <span class="ds-labels">Odleglosc</span>
+    <span id="distance">%DISTANCE%</span>
+  </p>
+</body>
+<script>
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("distance").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/distance", true);
+  xhttp.send();
+}, 10000) ;
+</script>
+</html>)rawliteral";
+
+
 int leftMotor = 25;
 int rigtMotor = 26;
 int delay_time = 800;  // Aprox 180 degrees
@@ -69,15 +118,48 @@ void sosSignal(int Pin) {
   delay(1000);
 }
 
+// Zastępuje placeholder wartościami DHT
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "DISTANCE"){
+    return String(CM);
+  }
+  return String();
+}
+
 void setup() {
+  Serial.begin(9600);
+    
+  // Łączenie z&nbsp;WiFi
+  WiFi.begin(ssid, password);
+  Serial.println("łączenie z&nbsp;WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  // Wyświetlanie lokalnego adresu IP ESP
+  Serial.println(WiFi.localIP());
+
+  // Przekierowanie do&nbsp;strony root / web
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+  request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/distance", HTTP_GET, [](AsyncWebServerRequest *request){
+  request->send_P(200, "text/plain", String(CM).c_str());
+  });
+
+  // Start server
+  server.begin();
+
+  
   pinMode(leftMotor, OUTPUT);
   pinMode(rigtMotor, OUTPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(micPin, INPUT);
   pinMode(SOSDiode, OUTPUT);
-
-  Serial.begin(9600);
 
   delay(delay_time * 2);
 }
